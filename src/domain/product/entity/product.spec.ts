2fb86cc4287d0a37e61cd59001db2f99037fe839
marcos-r-r <marcos@toup.dev>
2023-12-04 17:05:54 -0300

    feat: Notification pattern at Product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -13,6 +13,12 @@ describe("Product unit tests", () => {
     }).toThrowError("Name is required");
   });
 
+  it("should throw error when name is and id are empty", () => {
+    expect(() => {
+      let product = new Product("", "", 100);
+    }).toThrowError("Product: Id is required,Product: Name is required");
+  });
+
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
